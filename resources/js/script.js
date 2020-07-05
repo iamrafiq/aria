b32afe21928862654b9ab78f5119ffa2b1a44429
linuxrafiq <rafiq.i@live.co.uk>
2020-07-05 15:39:53 +0600

    working with tabs

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,22 +12,32 @@ $(document).ready(function(){
  });
 });
 
-function openTab(tabName) {
+function openTab(tabName, btnName) {
+    if(document.getElementById(tabName).classList.contains("tab-open")){
+        return;
+    }
+    
+    let y = document.getElementsByClassName("btn-tab");
+    for (let j = 0; j < y.length; j++) {
+     y[j].classList.remove("nav-tab-selected");
+     y[j].classList.add("nav-tab-unselected");
+
+    }
+    console.log(btnName);
+    document.getElementById(btnName).classList.add("nav-tab-selected");
+    document.getElementById(btnName).classList.remove("nav-tab-unselected");
+
+    //$(this).classList.add("nav-tab-selected");
+
     var i;
     var x = document.getElementsByClassName("tab");
     for (i = 0; i < x.length; i++) {
-     // x[i].style.display = "none";
      x[i].classList.remove("tab-open");
      x[i].classList.remove("fadeIn");
-
-
      x[i].classList.add("tab-close");
 
     }
-    console.log(tabName);
     document.getElementById(tabName).classList.add("tab-open");
     document.getElementById(tabName).classList.add("fadeIn");
-
     document.getElementById(tabName).classList.remove("tab-close");
-  //  document.getElementById(tabName).style.display = "block";
   }
